25ccf396a36e8f713ee70db0f8bd34983487d208
Rodrigo Orlandini <rodrigosorlandini@gmail.com>
2021-08-04 21:30:31 -0300

    Full project

---
+++ lib/inmana_web/controllers/restaurants_controller.ex
@@ -0,0 +1,16 @@
+defmodule InmanaWeb.RestaurantsController do
+  use InmanaWeb, :controller
+
+  alias Inmana.Restaurant
+  alias InmanaWeb.FallbackController
+
+  action_fallback FallbackController
+
+  def create(connection, params) do
+    with {:ok, %Restaurant{} = restaurant} <- Inmana.create_restaurant(params) do
+      connection
+      |> put_status(:created)
+      |> render("create.json", restaurant: restaurant)
+    end
+  end
+end
---
+++ lib/inmana_web/controllers/welcomer_controller.ex
@@ -0,0 +1,21 @@
+defmodule InmanaWeb.WelcomeController do
+  use InmanaWeb, :controller
+
+  alias Inmana.Welcomer
+
+  def index(connection, params) do
+    params
+    |> Welcomer.welcome()
+    |> handle_response(connection)
+  end
+
+  defp handle_response({:ok, message}, connection), do: render_response(connection, message, :ok)
+
+  defp handle_response({:error, message}, connection), do: render_response(connection, message, :bad_request)
+
+  defp render_response(connection, message, status) do
+    connection
+    |> put_status(status)
+    |> json(%{message: message})
+  end
+end
